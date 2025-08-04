865200c2b8551cd47e698fc7eb83aa68c794f0a5
lloydkev000 <lloydkevin631@gmail.com>
2025-08-04 13:50:16 -0400

    Yay :P

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-// TODO: make an arrow function named generateStory that takes in name, place, creature, and object as parameters.  It should return.  The following using template literals.
-
-// [name] traveled to the mysterious land of [place] and encountered a wild [creature].
-// Armed only with a magical [object], [name] had to think fast to survive the adventure! 🧙‍♂️✨`;
-
-// TODO: note you should use template strings where I've placed the [].  Also I've deliberately not used the right way to make literal.  Review the notes.
-
+const generateStory = (name, place, creature, object) => {
+    return `${name} traveled to the mysterious land of ${place} and encountered a wild ${creature}Armed only with a magical ${object} `
+};
 
 const listener = () => {
-    // TODO: use document.getElementById('name').value and assign to const name.
-    // TODO: repeat for place, creature, and object on their own lines.
+     document.getElementById('name').value = 'name';
+     document.getElementById('place').value =  'place';
+     document.getElementById('creature').value = 'creature';
+     document.getElementById('object').value = 'object';
 
-    // TODO: create a const story from generateStory() passing in name, place, creature, object.
-    // TODO: use document.getElementById('storyOutput').textContent = story;  to set the resulting paragraph tag with id storyOutput
+
+    document.getElementById('storyOutput').textContent = generateStory('name, place, creature, object');
 }
 
 document.getElementById('generateBtn').addEventListener('click', listener);
